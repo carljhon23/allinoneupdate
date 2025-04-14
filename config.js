c39fcc3f0e4e006be02652b79fcceb51a0b0420d
carljhon23 <carljhon879@gmail.com>
2025-04-14 21:32:09 +0100

    Update config.js

--- config.js
+++ config.js
@@ -4,10 +4,10 @@ module.exports = {
   ownerId: '123456789012345678',
   status: {
     rotateDefault: [
-      { name: 'Netflix', type: ActivityType.Watching },
-      { name: 'GTA VI', type: ActivityType.Playing },
-      { name: 'on YouTube', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
-      { name: 'Spotify', type: ActivityType.Listening },
+      { name: 'Wisdom Movies', type: ActivityType.Watching },
+      { name: 'Wisdom Games', type: ActivityType.Playing },
+      { name: 'Wisdom Live', type: ActivityType.Streaming, url: 'https://www.twitch.tv/glaceytt' },
+      { name: 'Apollo BElvedere', type: ActivityType.Listening },
     ],
     songStatus: true
   },
