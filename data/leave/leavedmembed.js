e3172fa69042294f5c77d3c39e4263a667eda4dc
carljhon23 <carljhon879@gmail.com>
2025-04-15 19:00:38 +0100

    Update leavedmembed.js

--- data/leave/leavedmembed.js
+++ data/leave/leavedmembed.js
@@ -3,8 +3,8 @@ const { EmbedBuilder } = require('discord.js');
 module.exports = function createLeaveDMEmbed(member) {
     return new EmbedBuilder()
         .setColor('#FF9900')
-        .setTitle('👋 You left a server')
-        .setDescription(`Goodbye from **${member.guild.name}**.\nWe hope to see you again!`)
+        .setTitle('👋 You left Wisdom Server')
+        .setDescription(`Goodbye from **${member.guild.name}**.\n mat3awdsh Trj3e Lhad server!`)
         .setThumbnail(member.user.displayAvatarURL({ dynamic: true }))
         .setTimestamp();
 };
