5d816594764b334ebecc6ea7c23d44e948f3ce99
carljhon23 <carljhon879@gmail.com>
2025-09-30 19:43:50 +0100

    Update checkPermissions.js

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -2,6 +2,7 @@
 const { serverConfigCollection } = require('../mongodb'); 
 const { EmbedBuilder } = require('discord.js');
 const cmdIcons = require('../UI/icons/commandicons');
+const { PermissionsBitField } = require('discord.js');
 
 module.exports = async function checkPermissions(interaction) {
     const guild = interaction.guild;
@@ -12,8 +13,10 @@ module.exports = async function checkPermissions(interaction) {
 
     const isOwner = interaction.user.id === guild.ownerId;
     const isBotManager = botManagers.includes(interaction.user.id);
+    const member = await guild.members.fetch(interaction.user.id);
+    const hasAdminRole = member.roles.cache.some(role => guild.roles.cache.get(role.id)?.permissions.has('Administrator'));
 
-    if (!isOwner && !isBotManager) {
+    if (!hasAdminRole && !isBotManager) {
         const embed = new EmbedBuilder()
             .setColor(0xFF0000)
             .setAuthor({
@@ -22,9 +25,9 @@ module.exports = async function checkPermissions(interaction) {
                 url: "https://discord.gg/xQF9f9yUEM"
             })
             .setDescription(
-                '- Only the **server owner** or **bot managers** can use this command.\n' +
-                '- If you believe this is a mistake, please contact the server owner or a bot manager.\n' +
-                '- If you are the server owner, please add User ID by running **/setup-serverconfig**.'
+                '- فقط من لديه صلاحية "Administrator" أو من مدراء البوت يمكنه استخدام هذا الأمر.\n' +
+                '- إذا كنت تعتقد أن هناك خطأ، يرجى التواصل مع مدير السيرفر أو مدير البوت.\n' +
+                '- إذا كنت مدير البوت، أضف معرف المستخدم عبر أمر **/setup-serverconfig**.'
             );
 
         await interaction.reply({
@@ -39,5 +42,12 @@ module.exports = async function checkPermissions(interaction) {
         return false;
     }
 
-    return true;
+    // تحقق من صلاحية الأدمن أو مالك السيرفر أو المدراء
+    if (
+        member.id === guild.ownerId ||
+        botManagers.includes(member.id) ||
+        member.permissions.has(PermissionsBitField.Flags.Administrator)
+    ) {
+        return true;
+    }
 };
