8b345a18b0a6e86da623b2dec502f3b98226a4db
carljhon23 <carljhon879@gmail.com>
2026-01-05 01:40:50 +0100

    Update lavalink.js

--- lavalink.js
+++ lavalink.js
@@ -2,10 +2,12 @@
 module.exports = {
     enabled: true, 
 "lavalink": {
-            "identifier": "Serenetia-V4",
+            "identifier": "NewLava",
             "password": "https://dsc.gg/ajidevserver",
-            "host": "lavalinkv4.serenetia.com",
-            "port": 443,
-  "secure": true
+            "host": "KaAs",
+            "port": 24590,
+  "secure": false
 }
 };
+
+#
